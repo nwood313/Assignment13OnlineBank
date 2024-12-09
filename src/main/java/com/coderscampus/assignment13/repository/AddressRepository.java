329ceb8595814486e4adbae19fa29e4501bc6dbc
nwood313 <161774016+nwood313@users.noreply.github.com>
2024-12-09 00:52:21 -0500

    Delete AddressRepository.java

--- src/main/java/com/coderscampus/assignment13/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coderscampus.assignment13.repository;
-
-import com.coderscampus.assignment13.domain.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
